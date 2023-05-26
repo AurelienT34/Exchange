0cadadeb48576c0362476e465b79ad2815ad3ae3
AurelienT34 <47206310+AurelienT34@users.noreply.github.com>
2023-05-26 15:59:44 +0200

    Update trading spot

--- src/components/Spot.vue
+++ src/components/Spot.vue
@@ -20,18 +20,21 @@
                 <button class="button" type="submit">Withdraw</button>
             </form>
         </div>
+        <br>
+        <OrdersBook/>
     </div>
 </template>
 
 <script>
 import { ethers } from 'ethers';
 import Wallet from '../artifacts/contracts/Wallet.sol/Wallet.json';
+import OrdersBook from './OrdersBook.vue'
 
 export default {
   data() {
     return {
         adresseWallet: null,
-        WalletAddress : "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // Adresse du contract
+        WalletAddress : "0x5FbDB2315678afecb367f032d93F642f64180aa3", // Adresse du contract
         ethAmount: null,
         usdtAmount: null,
         ethToAdd: null,
@@ -39,6 +42,9 @@ export default {
         withdrawAmount: null,
         isFormVisible: false,
     };
+  },
+  components : {
+    OrdersBook,
   },
   methods: {
 
